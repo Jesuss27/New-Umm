92ffe936cd60c9d6b7c64e6d69b534105da6b387
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-07-25 11:47:29 -0700

    Refactoring my new app

---
+++ packages/personal-theme/src/components/Testimonials.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { styled } from "frontity"
+
+export default function Testimonials() {
+    return(
+        <TestimonialsContainer>
+            
+        </TestimonialsContainer>
+    )
+}
