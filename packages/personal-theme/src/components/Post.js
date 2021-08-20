80333d6af6c797666d28d5ca7954771b4a7dd02f
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-08-20 11:12:03 -0700

    pagination component styling

--- packages/personal-theme/src/components/Post.js
+++ packages/personal-theme/src/components/Post.js
@@ -1,18 +1,103 @@
 import React from 'react'
-import { connect } from "frontity"
+import { connect, styled } from "frontity"
 
 function Post( { state } ) {
     const data = state.source.get(state.router.link);
     const post = state.source[data.type][data.id];
+    const fmedia = post.featured_media;
+    const target = state.source.attachment[fmedia]
+    const imgUrl = ( target ? target.source_url : "")
     
     
     return (
-        <div>
+        <StyledPost>
             <h1>{post.title.rendered}</h1>
+
+            <img src={imgUrl} alt="" />
+            
+            
             <p dangerouslySetInnerHTML={{__html: post.content.rendered}}></p>
             
-        </div>
+        </StyledPost>
     )
 }
 
 export default connect(Post)
+
+const StyledPost = styled.div`
+@media(min-width:1em){
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    padding:2rem;
+    background-color: #F8EDEB;
+
+
+    h1{
+        font-family:rockwell;
+        color:black;
+        font-size:var(--s-header);
+        padding:0rem 1rem;
+        margin-bottom: 2rem;
+        background:linear-gradient(to bottom, #F8EDEB 60%, #FEC89A 40%);
+
+    }
+    img{
+        width:50%;
+        height:30vh;
+        object-fit: cover;
+    }
+
+    p{
+        color:var(--color-text);
+        padding:1rem ;
+        width:100%;
+        font-size:1.4rem;
+    }
+}
+
+@media(min-width:600px){
+    h1{
+        font-size: var(--md-header);
+    }
+    p{
+        width:75%;
+        font-size: 2rem;
+        P{
+            width:100%;
+            line-height: 3.2rem;
+        }
+    }
+
+}
+
+@media(min-width:1000px){
+    h1{
+        font-size: var(--l-header);
+    }
+    p{
+        width:50%;
+        font-size: 2rem;
+        P{
+            width:100%;
+        }
+    }
+
+}
+
+@media(min-width:1400px){
+    h1{
+        font-size: 6rem;
+    }
+    p{
+        width:50%;
+        font-size: 2rem;
+        P{
+            width:100%;
+        }
+    }
+
+}
+
+`
+
